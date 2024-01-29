1d8d3c2485ce1eddfd1b8b9366f6b0e6d651ec37
Sakshi Ahire <88669971+ahiresakshi@users.noreply.github.com>
2024-01-29 16:17:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import App from "./components/App.js";
-import "./style.css";
-//import Form from "./components/Form.js";
-//import Signup from "./components/Signup";
-// import Json from "./components/Json";
-//import Window from "./components/Window";
-//import Game from "./components/Game";
-import Netflix from "./Netflix";
-// function App(){
-//     const [thingsArray, setThingsArray] =React.useState(["Thing1", "Thing2"])
-//     function addItem(){
-//         setThingsArray(prevThingsArray =>{
-//             return[...prevThingsArray, `Thing ${prevThingsArray.length+1}`]
-//         })
 
-//     }
-//     const thingsElements = thingsArray.map(thing=><p key={thing}>{thing}</p>)
-//     return(
-//         <div>
-//             <button onClick={addItem}>Add Item</button>
-//             {thingsElements}
-//         </div>
-//     )
-// }
-// ReactDOM.render(<App/>,document.getElementById("root"))
+import "./style.css";
+
+import Netflix from "./Netflix";
+
 function Index() {
   return <Netflix />;
 }
